559fde9404311ac0339a7766c20d8792c510ed78
Austin Workman <austin@hashicorp.com>
2019-06-12 22:41:55 -0500

    Adding map for instance type

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+  instance_type = "${lookup(var.instance_type, var.environment, "t2.micro")}"
   subnet_id     = "${lookup(local.subnets, var.environment, "fail")}"
 
   tags {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
 variable "org" {}
 variable "network_workspace" {}
 variable "environment" {}
+
+variable "instance_type" {
+  type = "map"
+
+  default = {
+    "prod"  = "m5.large"
+    "stage" = "t2.micro"
+    "dev"   = "t2.micro"
+  }
+}
